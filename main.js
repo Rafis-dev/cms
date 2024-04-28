b1a738b91beaa017d5d0cc0e2ad876a91c9e20df
Rafis Shigapov <rafis1work@gmail.com>
2024-04-28 20:26:07 +0300

    module 4 lesson 2

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 'use strict';
+{
+  const product = 'headphones';
+  const quantity = 17;
+  const category = 'audio';
+  const price = 7;
 
-const product = 'headphones';
-const quantity = 17;
-const category = 'audio';
-const price = 7;
+  console.log(product);
+  console.log(quantity * price);
+}
 
-console.log(product);
+{
+  const askProduct = prompt('Введите наименование товара', 'Колонки');
+  const askQuantity = prompt('Введите количество товара', '5');
+  const askCategory = prompt('Введите категорию товара', 'Аудио');
+  const askPrice = prompt('Введите цену товара', '400');
 
-console.log(quantity * price);
+  console.log(typeof askProduct);
+  console.log(typeof askQuantity);
+  console.log(typeof askCategory);
+  console.log(typeof askPrice);
+
+  const quantityToNum = +askQuantity;
+  const priceToNum = +askPrice;
+
+  console.log(typeof quantityToNum);
+  console.log(typeof priceToNum);
+
+  console.log(`На складе ${askQuantity} единиц(-ы) товара ${askProduct} на сумму ${askPrice * askQuantity} рубля(-ей)`);
+
+}
