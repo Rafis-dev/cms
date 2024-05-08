01f45af6fcb751778e69c762b9d87ed1c25dbd19
Rafis Shigapov <rafis1work@gmail.com>
2024-05-08 23:27:56 +0300

    module_4_lesson_3 hard_2 fixed

--- tasksHard_2.js
+++ tasksHard_2.js
@@ -3,22 +3,23 @@
 {
   const salary = prompt("Какой Ваш доход?", '15000');
   let tax = 0;
-  const salaryRemain = salary - 15000;
-  const salaryRemain2 = salary - 50000;
+  let taxRate = 0;
 
-  if (salary > 0 && salary <= 15000) {
-    tax = salary * 0.13;
-    console.log(`Налог на Ваш доход составляет ${tax} рублей`);
+
+  if (salary > 0) {
+
+    if (salary <= 15000) {
+      taxRate = 0.13;
+      tax = salary * taxRate;
+    }
+
+    if (salary <= 50000) {
+      taxRate = 0.2;
+      tax = (15000 * 0.13) + (salary - 15000) * taxRate;
+    } else {
+      taxRate = 0.3;
+      tax = (15000 * 0.13) + (50000 * 0.2) + (salary - 50000) * taxRate;
+    }
   }
-
-  if (salary > 15000 && salary <= 50000) {
-    tax = (15000 * 0.13) + (salaryRemain * 0.2);
-    console.log(`Налог на Ваш доход составляет ${tax} рублей`);
-  }
-
-  if (salary > 50000) {
-    tax = (15000 * 0.13) + (50000 * 0.2) + (salaryRemain2 * 0.3);
-    console.log(`Налог на Ваш доход составляет ${tax} рублей`);
-  }
-
+  console.log(`Налог на Ваш доход составляет ${tax} рублей`);
 }
