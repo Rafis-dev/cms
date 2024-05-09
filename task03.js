65c78eb51431f7f5b9be52455367f6dc1b1b914f
Rafis Shigapov <rafis1work@gmail.com>
2024-05-09 16:44:47 +0300

    module 4 lesson 4 main task

--- task03.js
+++ task03.js
@@ -1,9 +1,26 @@
 'use strict';
 
-const calculate = (sum, products, promo) => {
+const calculate = (totalSum, productQuantity, promo) => {
+  let sum = totalSum;
   let discount = 0;
-  if (products > 10) {
-    return discount;
+
+  if (productQuantity > 10) {
+    discount += sum * 0.03;
   }
 
+  if (sum > 30000) {
+    discount += (sum - 30000) * 0.15;
+  }
+
+  if (promo === 'METHED') {
+    discount += sum * 0.1;
+  }
+
+  if (promo === 'G3H2Z1' && sum - discount > 2000) {
+    discount += 500;
+  }
+
+  return sum -= discount;
 }
+
+console.log(calculate(100000, 20, 'METHED'));
