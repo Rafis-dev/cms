b1b1f955ec89cb1e24f883ded70b1d56bedf6718
Rafis Shigapov <rafis1work@gmail.com>
2024-05-18 15:37:13 +0300

    module 4 lesson 5

--- task03.js
+++ task03.js
@@ -1,2 +1,11 @@
 'use strict';
 
+const reverseStr = (str) => {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+console.log(reverseStr('Привет, мир'));
