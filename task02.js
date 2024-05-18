2d459dc8893da3b3989a538ff09413f2f85fb92f
Rafis Shigapov <rafis1work@gmail.com>
2024-05-18 13:50:56 +0300

    did task2

--- task02.js
+++ task02.js
@@ -1,6 +1,14 @@
 'use strict';
 
-const isPrime = () => { }
+const isPrime = (num) => {
+  if (num < 2) return false;
+
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+
+  return true;
+}
 
 
-
+console.log(isPrime());
