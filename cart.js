3d33f4e00605566606cb63e1c41d54320e2c3b40
Rafis Shigapov <rafis1work@gmail.com>
2024-06-08 15:21:22 +0300

    module 4 lesson 8

---
+++ cart.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const cart = {
+  items: [],
+  totalPrice: 0,
+  count: 0,
+
+  getTotalPrice() {
+    return this.totalPrice;
+  },
+
+  add(product, price, quantity = 1) {
+    const item = { product, price, quantity };
+    this.items.push(item);
+    this.increaseCount(quantity);
+    this.calculateItemPrice();
+  },
+
+  increaseCount(num) {
+    this.count += num;
+  },
+
+  calculateItemPrice() {
+    this.totalPrice = this.items.reduce((total, item) => total + item.price * item.quantity, 0);
+  },
+
+  clear() {
+    this.items = [];
+    this.totalPrice = 0;
+    this.count = 0;
+  },
+
+  print() {
+    console.log(JSON.stringify(this.items));
+    console.log(`Total Price: ${this.totalPrice}`);
+  },
+};
+
+
+cart.add("Apple", 1.5, 4);
+cart.add("Banana", 1.2, 3);
+cart.add("Orange", 2.0, 2);
+
+cart.print();
